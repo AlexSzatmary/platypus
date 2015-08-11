8cca3ee2da874b4a4363e191421974e31c857896
Alex C. Szatmary <alex.szatmary@gmail.com>
2015-08-11 11:57:05 -0400

    Shrink plots, other formatting changes

--- platypus.py
+++ platypus.py
@@ -82,7 +82,7 @@ class Figure(object):
 
         if figsize is None:
             if self.style == 'print':
-                panesize = (3.5, 4.)
+                panesize = (3., 3.)
             elif self.style == 'projector':
                 panesize = (8., 6.)
             elif style == 'poster':
@@ -324,21 +324,21 @@ def _boxplot_helper(
     boxprops={'color': 'black'}, capprops={'color': 'black'},
     flierprops={'color': 'black'}, meanprops={'color': 'black'},
     medianprops={'color': 'black'}, whiskerprops={'color': 'black'},
+    showfliers=False,
     **kwargs):
     ax = fig.fig.gca()
     ax.boxplot(
         x, boxprops=boxprops, capprops=capprops,
         flierprops=flierprops, meanprops=meanprops,
         medianprops=medianprops, whiskerprops=whiskerprops,
+        showfliers=showfliers,
         **kwargs)
 
 
-def boxplot(x, notch=False, sym='k.', vert=False, axes=None, **kwargs):
+def boxplot(x, notch=False, sym='k.', vert=True, axes=None, **kwargs):
     '''
     Easy default one-line function interface for making boxplots
     '''
-    if axes is None:
-        axes = [0.4,  0.25, 0.55,  0.65]
     def my_callback(fig, **kwargs):
         return _boxplot_helper(fig, x, **kwargs)
     fig = _plot(my_callback, axes=axes, notch=notch, sym=sym, vert=vert,
